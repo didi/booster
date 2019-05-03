3bd132cc734b4cfb9a56dfa7c79b8ed1b49b3593
johnsonlee <g.johnsonlee@gmail.com>
2019-05-03 23:49:23 +0800

    Generating report file for `booster-transform-shared-preferences`

--- booster-transform-shared-preferences/src/main/kotlin/com/didiglobal/booster/transform/sharedpreferences/SharedPreferencesEditorTransformer.kt
+++ booster-transform-shared-preferences/src/main/kotlin/com/didiglobal/booster/transform/sharedpreferences/SharedPreferencesEditorTransformer.kt
@@ -1,23 +1,35 @@
 package com.didiglobal.booster.transform.sharedpreferences
 
-import com.didiglobal.booster.kotlinx.GREEN
-import com.didiglobal.booster.kotlinx.RESET
 import com.didiglobal.booster.kotlinx.asIterable
+import com.didiglobal.booster.kotlinx.file
+import com.didiglobal.booster.kotlinx.touch
 import com.didiglobal.booster.transform.TransformContext
 import com.didiglobal.booster.transform.asm.ClassTransformer
+import com.didiglobal.booster.transform.shared.preferences.Build
 import com.google.auto.service.AutoService
 import jdk.internal.org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.MethodInsnNode
 import org.objectweb.asm.tree.MethodNode
+import java.io.PrintWriter
 
 /**
- * Represents a transformer for *SharedPreferences* bug fixing
+ * Represents a transformer for *SharedPreferences* optimization
  *
  * @author johnsonlee
  */
 @AutoService(ClassTransformer::class)
 class SharedPreferencesEditorTransformer : ClassTransformer {
+
+    private lateinit var logger: PrintWriter
+
+    override fun onPreTransform(context: TransformContext) {
+        this.logger = context.reportsDir.file(Build.ARTIFACT).file(context.name).file("report.txt").touch().printWriter()
+    }
+
+    override fun onPostTransform(context: TransformContext) {
+        this.logger.close()
+    }
 
     override fun transform(context: TransformContext, klass: ClassNode): ClassNode {
         if (klass.name == SHADOW_EDITOR) {
@@ -29,34 +41,28 @@ class SharedPreferencesEditorTransformer : ClassTransformer {
                 it.opcode == Opcodes.INVOKEINTERFACE && it.owner == SHARED_PREFERENCES_EDITOR
             }?.forEach { invoke ->
                 when ("${invoke.name}${invoke.desc}") {
-                    "commit()Z" -> {
+                    "commit()Z" -> if (Opcodes.POP == invoke.next?.opcode) {
                         // if the return value of commit() does not used
                         // use asynchronous commit() instead
-                        if (Opcodes.POP == invoke.next?.opcode) {
-                            optimize(invoke, klass, method)
-                            method.instructions.remove(invoke.next)
-                        }
+                        invoke.optimize(klass, method)
+                        method.instructions.remove(invoke.next)
                     }
-                    "apply()V" -> {
-                        optimize(invoke, klass, method)
-                    }
+                    "apply()V" -> invoke.optimize(klass, method)
                 }
             }
         }
         return klass
     }
 
-}
-
-private fun optimize(invoke: MethodInsnNode, klass: ClassNode, method: MethodNode) {
-    println(" * $GREEN${invoke.owner}.${invoke.name}${invoke.desc}$RESET => $GREEN$SHADOW_EDITOR.apply(L$SHARED_PREFERENCES_EDITOR;)V$RESET: ${klass.name}.${method.name}${method.desc}")
-    invoke.apply {
-        itf = false
-        owner = SHADOW_EDITOR
-        name = "apply"
-        opcode = Opcodes.INVOKESTATIC
-        desc = "(L$SHARED_PREFERENCES_EDITOR;)V"
+    private fun MethodInsnNode.optimize(klass: ClassNode, method: MethodNode) {
+        logger.println(" * ${this.owner}.${this.name}${this.desc} => $SHADOW_EDITOR.apply(L$SHARED_PREFERENCES_EDITOR;)V: ${klass.name}.${method.name}${method.desc}")
+        this.itf = false
+        this.owner = SHADOW_EDITOR
+        this.name = "apply"
+        this.opcode = Opcodes.INVOKESTATIC
+        this.desc = "(L$SHARED_PREFERENCES_EDITOR;)V"
     }
+
 }
 
 private const val SHARED_PREFERENCES_EDITOR = "android/content/SharedPreferences\$Editor"
