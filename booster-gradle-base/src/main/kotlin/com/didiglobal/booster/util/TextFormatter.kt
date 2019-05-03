5d934449d5a8c4dcd7403041d8b03eb812134f19
johnsonlee <g.johnsonlee@gmail.com>
2019-05-03 14:08:54 +0800

    Update log message format of TextFormatter

--- booster-gradle-base/src/main/kotlin/com/didiglobal/booster/util/TextFormatter.kt
+++ booster-gradle-base/src/main/kotlin/com/didiglobal/booster/util/TextFormatter.kt
@@ -3,11 +3,14 @@ package com.didiglobal.booster.util
 import java.util.logging.LogRecord
 import java.util.logging.SimpleFormatter
 
+/**
+ * Represents a log message formatter
+ *
+ * @author johnsonlee
+ */
 class TextFormatter : SimpleFormatter() {
 
     @Synchronized
-    override fun format(record: LogRecord): String {
-        return "${record.message}\n"
-    }
+    override fun format(record: LogRecord) = "${record.level.localizedName}: ${record.message}\n"
 
 }
