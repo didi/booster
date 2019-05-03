49ad333a1f407ba6eda7c839989c757c4bb95914
johnsonlee <g.johnsonlee@gmail.com>
2019-05-03 23:50:05 +0800

    Generating report file for `booster-transform-toast`

--- booster-transform-toast/src/main/kotlin/com/didiglobal/booster/transform/toast/ToastTransformer.kt
+++ booster-transform-toast/src/main/kotlin/com/didiglobal/booster/transform/toast/ToastTransformer.kt
@@ -1,14 +1,16 @@
 package com.didiglobal.booster.transform.toast
 
-import com.didiglobal.booster.kotlinx.GREEN
-import com.didiglobal.booster.kotlinx.RESET
 import com.didiglobal.booster.kotlinx.asIterable
+import com.didiglobal.booster.kotlinx.file
+import com.didiglobal.booster.kotlinx.touch
 import com.didiglobal.booster.transform.TransformContext
 import com.didiglobal.booster.transform.asm.ClassTransformer
 import com.google.auto.service.AutoService
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.MethodInsnNode
+import org.objectweb.asm.tree.MethodNode
+import java.io.PrintWriter
 
 /**
  * Represents a class node transformer used to fix [bug: 30150688](https://android.googlesource.com/platform/frameworks/base/+/dc24f93)
@@ -18,6 +20,16 @@ import org.objectweb.asm.tree.MethodInsnNode
 @AutoService(ClassTransformer::class)
 class ToastTransformer : ClassTransformer {
 
+    private lateinit var logger: PrintWriter
+
+    override fun onPreTransform(context: TransformContext) {
+        this.logger = context.reportsDir.file(Build.ARTIFACT).file(context.name).file("report.txt").touch().printWriter()
+    }
+
+    override fun onPostTransform(context: TransformContext) {
+        this.logger.close()
+    }
+
     override fun transform(context: TransformContext, klass: ClassNode): ClassNode {
         if (klass.name == SHADOW_TOAST) {
             return klass
@@ -26,18 +38,20 @@ class ToastTransformer : ClassTransformer {
         klass.methods.forEach { method ->
             method.instructions?.iterator()?.asIterable()?.filterIsInstance(MethodInsnNode::class.java)?.filter {
                 it.owner == TOAST && it.name == "show" && it.desc == "()V"
-            }?.forEach { invoke ->
-                println(" * $GREEN${invoke.owner}.${invoke.name}${invoke.desc}$RESET => $GREEN$SHADOW_TOAST.apply(L$SHADOW_TOAST;)V: ${klass.name}.${method.name}${method.desc}$RESET")
-                invoke.apply {
-                    owner = SHADOW_TOAST
-                    name = "show"
-                    desc = "(L$TOAST;)V"
-                    opcode = Opcodes.INVOKESTATIC
-                    itf = false
-                }
+            }?.forEach {
+                it.optimize(klass, method)
             }
         }
         return klass
+    }
+
+    private fun MethodInsnNode.optimize(klass: ClassNode, method: MethodNode) {
+        println(" * ${this.owner}.${this.name}${this.desc} => $SHADOW_TOAST.apply(L$SHADOW_TOAST;)V: ${klass.name}.${method.name}${method.desc}")
+        this.owner = SHADOW_TOAST
+        this.name = "show"
+        this.desc = "(L$TOAST;)V"
+        this.opcode = Opcodes.INVOKESTATIC
+        this.itf = false
     }
 
 }
