4d84df5925a245aa7a191363b9ce359de769879c
johnsonlee <g.johnsonlee@gmail.com>
2019-05-11 01:12:21 +0800

    Add tuple extension

---
+++ booster-kotlinx/src/main/kotlin/com/didiglobal/booster/kotlinx/tuple.kt
@@ -0,0 +1,11 @@
+package com.didiglobal.booster.kotlinx
+
+data class Quadruple<A, B, C, D>(val first: A, val second: B, val third: C, val fourth: D)
+
+data class Quintuple<A, B, C, D, E>(val first: A, val second: B, val third: C, val fourth: D, val fifth: E)
+
+data class Sextuple<A, B, C, D, E, F>(val first: A, val second: B, val third: C, val fourth: D, val fifth: E, val sixth: F)
+
+data class Septuple<A, B, C, D, E, F, G>(val first: A, val second: B, val third: C, val fourth: D, val fifth: E, val sixth: F, val seventh: G)
+
+data class Octuple<A, B, C, D, E, F, G, H>(val first: A, val second: B, val third: C, val fourth: D, val fifth: E, val sixth: F, val seventh: G, val eighth: H)
