450fbb5f6d5e85f5bee7771c1ed473062cb978f0
neighbWang <wangzhiguo13120140@163.com>
2019-06-05 16:46:29 +0800

    AsyncTask optimization

---
+++ booster-android-instrument-thread/src/main/java/com/didiglobal/booster/instrument/ShadowAsyncTask.java
@@ -0,0 +1,26 @@
+package com.didiglobal.booster.instrument;
+
+import android.util.Log;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static com.didiglobal.booster.android.bugfix.Constants.TAG;
+
+/**
+ * @author neighbWang
+ */
+public class ShadowAsyncTask {
+
+    /**
+     * Optimize the thread pool executor of AsyncTask with {@code allowCoreThreadTimeOut = true}
+     */
+    public static void optimizeAsyncTaskExecutor() {
+        try {
+            final ThreadPoolExecutor executor = ((ThreadPoolExecutor) android.os.AsyncTask.THREAD_POOL_EXECUTOR);
+            executor.allowCoreThreadTimeOut(true);
+            Log.i(TAG, "Optimize AsyncTask executor success！");
+        } catch (final Throwable t) {
+            Log.e(TAG, "Optimize AsyncTask executor error: allowCoreThreadTimeOut = true", t);
+        }
+    }
+}
