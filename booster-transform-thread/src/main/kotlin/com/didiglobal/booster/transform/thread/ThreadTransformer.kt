89f46fd7e71f58dbc20b4d39f9bc00148f0831ec
neighbWang <wangzhiguo13120140@163.com>
2019-06-05 16:26:28 +0800

    AsyncTask optimization

--- booster-transform-thread/src/main/kotlin/com/didiglobal/booster/transform/thread/ThreadTransformer.kt
+++ booster-transform-thread/src/main/kotlin/com/didiglobal/booster/transform/thread/ThreadTransformer.kt
@@ -3,11 +3,14 @@ package com.didiglobal.booster.transform.thread
 import com.didiglobal.booster.kotlinx.asIterable
 import com.didiglobal.booster.kotlinx.file
 import com.didiglobal.booster.kotlinx.touch
+import com.didiglobal.booster.transform.ArtifactManager
 import com.didiglobal.booster.transform.TransformContext
 import com.didiglobal.booster.transform.asm.ClassTransformer
 import com.didiglobal.booster.transform.asm.className
 import com.didiglobal.booster.transform.asm.find
+import com.didiglobal.booster.transform.asm.findAll
 import com.didiglobal.booster.transform.asm.isInstanceOf
+import com.didiglobal.booster.util.ComponentHandler
 import com.google.auto.service.AutoService
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
@@ -17,6 +20,7 @@ import org.objectweb.asm.tree.MethodInsnNode
 import org.objectweb.asm.tree.MethodNode
 import org.objectweb.asm.tree.TypeInsnNode
 import java.io.PrintWriter
+import javax.xml.parsers.SAXParserFactory
 
 /**
  * Represents a class transformer for multithreading optimization
@@ -27,8 +31,16 @@ import java.io.PrintWriter
 class ThreadTransformer : ClassTransformer {
 
     private lateinit var logger: PrintWriter
+    private val applications = mutableSetOf<String>()
 
     override fun onPreTransform(context: TransformContext) {
+        val parser = SAXParserFactory.newInstance().newSAXParser()
+        context.artifacts.get(ArtifactManager.MERGED_MANIFESTS).forEach { manifest ->
+            val handler = ComponentHandler()
+            parser.parse(manifest, handler)
+            applications.addAll(handler.applications)
+        }
+
         this.logger = context.reportsDir.file(Build.ARTIFACT).file(context.name).file("report.txt").touch().printWriter()
     }
 
@@ -39,6 +51,10 @@ class ThreadTransformer : ClassTransformer {
     override fun transform(context: TransformContext, klass: ClassNode): ClassNode {
         if (klass.name.startsWith(SHADOW)) {
             return klass
+        }
+
+        if (this.applications.contains(klass.className)) {
+            optimizeAsyncTask(klass)
         }
 
         klass.methods?.forEach { method ->
@@ -180,9 +196,31 @@ private fun MethodInsnNode.transformInvokeVirtual(context: TransformContext, kla
         }
     }
 
+    private fun optimizeAsyncTask(klass: ClassNode) {
+        val method = klass.methods?.find {
+            "${it.name}${it.desc}" == "<clinit>()V"
+        } ?: klass.defaultCInit
+
+        method.instructions?.let { insn ->
+            insn.findAll(Opcodes.RETURN, Opcodes.ATHROW).forEach {
+                insn.insertBefore(it, MethodInsnNode(Opcodes.INVOKESTATIC, SHADOW_ASYNC_TASK, "optimizeAsyncTaskExecutor", "()V", false))
+                logger.println(" + $SHADOW_ASYNC_TASK.optimizeAsyncTaskExecutor()V before @${if (it.opcode == Opcodes.ATHROW) "athrow" else "return"}: ${klass.name}.${method.name}${method.desc} ")
+
+            }
+        }
+    }
+
 }
 
 private fun makeThreadName(name: String) = MARK + name
+
+private val ClassNode.defaultCInit: MethodNode
+    get() = MethodNode(Opcodes.ACC_STATIC, "<clinit>", "()V", null, null).apply {
+        maxStack = 1
+        instructions.insert(InsnNode(Opcodes.RETURN))
+        methods?.add(this)
+    }
+
 
 internal val MARK = "\u200B"
 
@@ -192,6 +230,7 @@ const val SHADOW_THREAD = "${SHADOW}Thread"
 const val SHADOW_TIMER = "${SHADOW}Timer"
 const val SHADOW_EXECUTORS = "${SHADOW}Executors"
 const val SHADOW_THREAD_POOL_EXECUTOR = "${SHADOW}ThreadPoolExecutor"
+const val SHADOW_ASYNC_TASK = "${SHADOW}AsyncTask"
 
 const val HANDLER_THREAD = "android/os/HandlerThread"
 const val THREAD = "java/lang/Thread"
