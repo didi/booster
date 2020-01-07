fc7b7eb140e26b83ab3a6cd4f650a919cb12a5f8
johnsonlee <g.johnsonlee@gmail.com>
2020-01-08 02:45:47 +0800

    Add utility for class file import analysing

---
+++ booster-transform-util/src/test/kotlin/com/didiglobal/booster/transform/util/ImportAnalyserTest.kt
@@ -0,0 +1,27 @@
+package com.didiglobal.booster.transform.util
+
+import java.io.InputStream
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class ImportAnalyserTest {
+
+    private lateinit var bytecode: InputStream
+
+    @BeforeTest
+    fun setup() {
+        bytecode = javaClass.classLoader.getResourceAsStream("Configuration.class")!!
+    }
+
+    @Test
+    fun analyse() {
+        ImportAnalyser(bytecode).analyse().forEach(::println)
+    }
+
+    @AfterTest
+    fun teardown() {
+        bytecode.close()
+    }
+
+}
