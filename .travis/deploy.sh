280bf838f1d2b9ffff49616bc205d4b39a5e8391
johnsonlee <g.johnsonlee@gmail.com>
2020-01-11 21:35:40 +0800

    Fix Travis CI build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 ./gradlew uploadArchives \
+    -POSSRH_USERNAME=${OSSRH_USERNAME} \
+    -POSSRH_PASSWORD=${OSSRH_PASSWORD} \
     -Psigning.keyId=${GPG_KEY_ID} \
     -Psigning.password=${GPG_PASSPHRASE} \
     -Psigning.secretKeyRingFile=${TRAVIS_BUILD_DIR}/pubring.gpg
