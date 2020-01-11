6603f3cbd41593e74e5af9ddaedde94217a670d5
johnsonlee <g.johnsonlee@gmail.com>
2020-01-12 03:18:30 +0800

    Update Travis CI configuration

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-./gradlew uploadArchives \
+./gradlew publishToSonatype \
     -POSSRH_USERNAME=${OSSRH_USERNAME} \
     -POSSRH_PASSWORD=${OSSRH_PASSWORD} \
     -Psigning.keyId=${GPG_KEY_ID} \
     -Psigning.password=${GPG_PASSPHRASE} \
-    -Psigning.secretKeyRingFile=${TRAVIS_BUILD_DIR}/secring.gpg
+    -Psigning.secretKeyRingFile=${TRAVIS_BUILD_DIR}/.travis/secring.gpg
 
