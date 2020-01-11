0e617c46ea4eea3826eda375a0cdddf72e48eeec
johnsonlee <g.johnsonlee@gmail.com>
2020-01-11 22:26:52 +0800

    Update Travis CI configuration

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,5 +5,5 @@
     -POSSRH_PASSWORD=${OSSRH_PASSWORD} \
     -Psigning.keyId=${GPG_KEY_ID} \
     -Psigning.password=${GPG_PASSPHRASE} \
-    -Psigning.secretKeyRingFile=${TRAVIS_BUILD_DIR}/pubring.gpg
+    -Psigning.secretKeyRingFile=${TRAVIS_BUILD_DIR}/secring.gpg
 
