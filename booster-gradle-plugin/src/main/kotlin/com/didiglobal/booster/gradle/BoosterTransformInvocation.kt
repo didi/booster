a33c63ecd188e74c98841bc84c20cb873ea56e33
linjiang <linjiang.me@gmail.com>
2020-03-14 12:33:37 +0800

    Fix incremental transform bug
    增量编译时，对于REMOVED状态的file，应该删除outputProvider下对应的dest文件而不是src

--- booster-gradle-plugin/src/main/kotlin/com/didiglobal/booster/gradle/BoosterTransformInvocation.kt
+++ booster-gradle-plugin/src/main/kotlin/com/didiglobal/booster/gradle/BoosterTransformInvocation.kt
@@ -158,12 +158,13 @@ internal class BoosterTransformInvocation(private val delegate: TransformInvocat
 
     @Suppress("NON_EXHAUSTIVE_WHEN")
     private fun doIncrementalTransform(jarInput: JarInput) {
+        val output = outputProvider.getContentLocation(jarInput.name, jarInput.contentTypes, jarInput.scopes, Format.JAR)
         when (jarInput.status) {
-            REMOVED -> jarInput.file.delete()
+            REMOVED -> output.delete()
             CHANGED, ADDED -> {
                 project.logger.info("Transforming ${jarInput.file}")
                 outputProvider?.let { provider ->
-                    jarInput.transform(provider.getContentLocation(jarInput.name, jarInput.contentTypes, jarInput.scopes, Format.JAR), this)
+                    jarInput.transform(output, this)
                 }
             }
         }
@@ -171,14 +172,14 @@ internal class BoosterTransformInvocation(private val delegate: TransformInvocat
 
     @Suppress("NON_EXHAUSTIVE_WHEN")
     private fun doIncrementalTransform(dirInput: DirectoryInput, base: URI) {
+        val root = provider.getContentLocation(dirInput.name, dirInput.contentTypes, dirInput.scopes, Format.DIRECTORY)
         dirInput.changedFiles.forEach { (file, status) ->
+            val output = File(root, base.relativize(file.toURI()).path)
             when (status) {
-                REMOVED -> file.delete()
+                REMOVED -> output.deleteRecursively()
                 ADDED, CHANGED -> {
                     project.logger.info("Transforming $file")
                     outputProvider?.let { provider ->
-                        val root = provider.getContentLocation(dirInput.name, dirInput.contentTypes, dirInput.scopes, Format.DIRECTORY)
-                        val output = File(root, base.relativize(file.toURI()).path)
                         file.transform(output) { bytecode ->
                             bytecode.transform(this)
                         }
