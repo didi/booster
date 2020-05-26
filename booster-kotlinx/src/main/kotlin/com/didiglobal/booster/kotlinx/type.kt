fc51b25d44e66c4b1013d6229c2d6a606422c3cc
johnsonlee <g.johnsonlee@gmail.com>
2020-05-27 01:55:36 +0800

    refactor booster-kotlinx

--- booster-kotlinx/src/main/kotlin/com/didiglobal/booster/kotlinx/type.kt
+++ booster-kotlinx/src/main/kotlin/com/didiglobal/booster/kotlinx/type.kt
@@ -2,8 +2,28 @@ package com.didiglobal.booster.kotlinx
 
 import java.lang.reflect.Method
 
+private val SIGNATURES = mapOf(
+        Boolean::class.java to "Z",
+        Byte::class.java    to "B",
+        Char::class.java    to "C",
+        Short::class.java   to "S",
+        Int::class.java     to "I",
+        Float::class.java   to "F",
+        Double::class.java  to "D",
+        Long::class.java    to "J",
+        Void.TYPE           to "V"
+)
+
 val Class<*>.descriptor: String
-    get() = Types.getDescriptor(this)
+    get() {
+        val signature = SIGNATURES[this]
+        if (null != signature) {
+            return signature
+        }
+        return if (isArray) {
+            "[" + componentType.descriptor
+        } else "L" + name.replace('.', '/') + ";"
+    }
 
 val Method.descriptor: String
     get() = parameters.joinToString("", "(", ")${returnType.descriptor}") {
