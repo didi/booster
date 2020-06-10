d0cb0908914f3a456ee947bd69c7cef7a7124349
johnsonlee <g.johnsonlee@gmail.com>
2020-06-11 01:18:12 +0800

    Refactor VariantProcessor loading

--- booster-android-gradle-api/src/main/kotlin/com/didiglobal/booster/gradle/TransformManager.kt
+++ booster-android-gradle-api/src/main/kotlin/com/didiglobal/booster/gradle/TransformManager.kt
@@ -5,6 +5,8 @@ import com.android.build.gradle.internal.pipeline.TransformManager
 
 val SCOPE_PROJECT: MutableSet<in QualifiedContent.Scope> = TransformManager.PROJECT_ONLY
 
+val SCOPE_FULL_PROJECT: MutableSet<in QualifiedContent.Scope> = TransformManager.SCOPE_FULL_PROJECT
+
 val SCOPE_FULL_WITH_FEATURES: MutableSet<in QualifiedContent.Scope> = when {
     GTE_V3_2 -> TransformManagerV32.SCOPE_FULL_WITH_FEATURES
     else -> TransformManager.SCOPE_FULL_PROJECT
