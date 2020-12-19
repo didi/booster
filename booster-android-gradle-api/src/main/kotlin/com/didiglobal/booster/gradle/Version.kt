5a8a0db82c973bce782bc4212fdaf7d035aa0a3e
johnsonlee <g.johnsonlee@gmail.com>
2020-12-19 15:26:01 +0800

    Fix AGP version comparison

--- booster-android-gradle-api/src/main/kotlin/com/didiglobal/booster/gradle/Version.kt
+++ booster-android-gradle-api/src/main/kotlin/com/didiglobal/booster/gradle/Version.kt
@@ -12,19 +12,21 @@ import com.didiglobal.booster.android.gradle.v4_0.V40
 import com.didiglobal.booster.android.gradle.v4_1.V41
 import com.didiglobal.booster.android.gradle.v4_2.V42
 
-internal val ANDROID_GRADLE_PLUGIN_VERSION = Revision.parseRevision(Version.ANDROID_GRADLE_PLUGIN_VERSION)
+internal val ANDROID_GRADLE_PLUGIN_VERSION: Revision = Revision.parseRevision(Version.ANDROID_GRADLE_PLUGIN_VERSION)
+internal val MAJOR = ANDROID_GRADLE_PLUGIN_VERSION.major
+internal val MINOR = ANDROID_GRADLE_PLUGIN_VERSION.minor
 
-val GTE_V3_X = ANDROID_GRADLE_PLUGIN_VERSION.major >= 3
-val GTE_V3_6 = GTE_V3_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 6
-val GTE_V3_5 = GTE_V3_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 5
-val GTE_V3_4 = GTE_V3_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 4
-val GTE_V3_3 = GTE_V3_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 3
-val GTE_V3_2 = GTE_V3_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 2
-val GTE_V3_1 = GTE_V3_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 1
+val GTE_V3_X = MAJOR >= 3
+val GTE_V3_6 = MAJOR > 3 || (MAJOR == 3 && MINOR >= 6)
+val GTE_V3_5 = MAJOR > 3 || (MAJOR == 3 && MINOR >= 5)
+val GTE_V3_4 = MAJOR > 3 || (MAJOR == 3 && MINOR >= 4)
+val GTE_V3_3 = MAJOR > 3 || (MAJOR == 3 && MINOR >= 3)
+val GTE_V3_2 = MAJOR > 3 || (MAJOR == 3 && MINOR >= 2)
+val GTE_V3_1 = MAJOR > 3 || (MAJOR == 3 && MINOR >= 1)
 
-val GTE_V4_X = ANDROID_GRADLE_PLUGIN_VERSION.major >= 4
-val GTE_V4_1 = GTE_V4_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 1
-val GTE_V4_2 = GTE_V4_X && ANDROID_GRADLE_PLUGIN_VERSION.minor >= 2
+val GTE_V4_X = MAJOR >= 4
+val GTE_V4_2 = MAJOR > 4 || (MAJOR == 4 && MINOR >= 2)
+val GTE_V4_1 = MAJOR > 4 || (MAJOR == 4 && MINOR >= 1)
 
 internal val AGP: AGPInterface = arrayOf(
         GTE_V4_2 to V42,
