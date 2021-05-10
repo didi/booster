85db5847d2b83ac42f959fec5a47ee9104b09f46
johnsonlee <g.johnsonlee@gmail.com>
2021-05-11 02:16:27 +0800

    Drop travis CI

--- .travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-./gradlew publishToSonatype \
-    -POSSRH_USERNAME=${OSSRH_USERNAME} \
-    -POSSRH_PASSWORD=${OSSRH_PASSWORD} \
-    -Psigning.keyId=${GPG_KEY_ID} \
-    -Psigning.password=${GPG_PASSPHRASE} \
-    -Psigning.secretKeyRingFile=${TRAVIS_BUILD_DIR}/.travis/secring.gpg
-
