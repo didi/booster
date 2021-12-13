2084282bc3ada9762c7f4576e2f3f1c125717aa4
johnsonlee <g.johnsonlee@gmail.com>
2021-12-13 22:43:10 +0800

    Add Android stub APIs

--- booster-android-api/src/main/java/android/content/pm/PackageItemInfo.java
+++ booster-android-api/src/main/java/android/content/pm/PackageItemInfo.java
@@ -1,4 +1,29 @@
 package android.content.pm;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
 public class PackageItemInfo {
+
+    public int banner;
+
+    public int icon;
+
+    public int labelRes;
+
+    public int logo;
+
+    public Bundle metaData;
+
+    public String name;
+
+    public CharSequence nonLocalizedLabel;
+
+    public String packageName;
+
+    public Drawable loadIcon (PackageManager pm) {
+        throw new RuntimeException("Stub!");
+    }
+
+
 }
