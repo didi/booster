2e5a4c4194153687b88139732e3591445bb84f06
johnsonlee <g.johnsonlee@gmail.com>
2022-04-26 12:46:48 +0800

    Add `DotGraph.visualize()` for graph visualization

--- booster-task-graph/src/main/kotlin/com/didiglobal/booster/task/graph/TaskGraphVariantProcessor.kt
+++ booster-task-graph/src/main/kotlin/com/didiglobal/booster/task/graph/TaskGraphVariantProcessor.kt
@@ -3,18 +3,11 @@ package com.didiglobal.booster.task.graph
 import com.android.build.gradle.api.BaseVariant
 import com.didiglobal.booster.cha.graph.CallGraph
 import com.didiglobal.booster.cha.graph.dot.DotGraph
-import com.didiglobal.booster.command.CommandService
 import com.didiglobal.booster.gradle.project
-import com.didiglobal.booster.kotlinx.OS
-import com.didiglobal.booster.kotlinx.execute
 import com.didiglobal.booster.kotlinx.file
 import com.didiglobal.booster.task.spi.VariantProcessor
 import com.google.auto.service.AutoService
-import java.io.BufferedReader
-import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
-
-private val DOT = "dot${OS.executableSuffix}"
 
 @AutoService(VariantProcessor::class)
 class TaskGraphVariantProcessor : VariantProcessor {
@@ -41,20 +34,11 @@ class TaskGraphVariantProcessor : VariantProcessor {
                 builder
             }.build()
 
-            // write dot file
-            dot.writeText(DotGraph.DIGRAPH.render(graph).toString())
-
-            // convert dot to png
-            CommandService.fromPath(DOT).location.file.let(::File).takeIf(File::exists)?.let {
-                val cmdline = "${it.canonicalPath} -Tpng -O ${dot.canonicalPath}"
-                project.logger.info(cmdline)
-                cmdline.execute()
-            }?.let { p ->
-                p.waitFor()
-                if (p.exitValue() != 0) {
-                    project.logger.error(p.errorStream.bufferedReader().use(BufferedReader::readText))
-                }
-            } ?: project.logger.warn("Command `${DOT}` not found")
+            try {
+                DotGraph.DIGRAPH.visualize(graph, dot)
+            } catch (e: Exception) {
+                project.logger.error(e.message)
+            }
         }
     }
 
