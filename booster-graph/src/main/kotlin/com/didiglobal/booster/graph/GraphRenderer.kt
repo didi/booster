4f3719a3b004b519da49cc734d5cd55dad0f16df
johnsonlee <g.johnsonlee@gmail.com>
2022-05-17 11:07:22 +0800

    Refactoring `booster-graph` to support grouping and render options

--- booster-graph/src/main/kotlin/com/didiglobal/booster/graph/GraphRenderer.kt
+++ booster-graph/src/main/kotlin/com/didiglobal/booster/graph/GraphRenderer.kt
@@ -7,6 +7,14 @@ package com.didiglobal.booster.graph
  */
 interface GraphRenderer {
 
-    fun <N : Node> render(graph: Graph<N>, prettify: (N) -> String = Node::toPrettyString): CharSequence
+    fun <N : Node> render(graph: Graph<N>, options: Options = EmptyOptions, prettify: (N) -> String = Node::toPrettyString): CharSequence
+
+    interface Options {
+        operator fun get(name: String): String?
+    }
+
+    object EmptyOptions : Options {
+        override fun get(name: String): String? = null
+    }
 
 }
