f49ca8263270e6b257e118e2a5d41bd6878e7d97
johnsonlee <g.johnsonlee@gmail.com>
2022-06-03 23:04:05 +0800

    Improve ClassSet to support loading classes from AAR

--- booster-cha/src/test/kotlin/com/didiglobal/booster/cha/ClassSetTest.kt
+++ booster-cha/src/test/kotlin/com/didiglobal/booster/cha/ClassSetTest.kt
@@ -1,31 +1,47 @@
 package com.didiglobal.booster.cha
 
-import com.didiglobal.booster.cha.Build
+import com.didiglobal.booster.transform.asm.asClassNode
 import com.didiglobal.booster.transform.asm.className
+import org.objectweb.asm.tree.ClassNode
 import java.io.File
+import java.io.InputStream
 import kotlin.test.Test
 import kotlin.test.assertTrue
+
+object AsmClassFileParser : ClassFileParser<ClassNode> {
+
+    override fun parse(input: InputStream): ClassNode = input.asClassNode()
+
+    override fun getAccessFlags(classNode: ClassNode): Int = classNode.access
+
+    override fun getInterfaces(classNode: ClassNode): Array<String> = classNode.interfaces.toTypedArray()
+
+    override fun getSuperName(classNode: ClassNode): String? = classNode.superName
+
+    override fun getClassName(classNode: ClassNode): String = classNode.name
+
+}
 
 class ClassSetTest {
 
     @Test
     fun `create directory backed class archive`() {
         val classes = File(javaClass.protectionDomain.codeSource.location.file)
-        assertTrue(ClassSet.from(classes).mapNotNull { it.className }.toSet().isNotEmpty())
+        assertTrue(ClassSet.from(classes, AsmClassFileParser).mapNotNull { it.className }.toSet().isNotEmpty())
     }
 
     @Test
     fun `create jar backed class archive`() {
         val jar = File(javaClass.protectionDomain.codeSource.location.file).resolve("..${File.separator}..${File.separator}..${File.separator}libs${File.separator}${Build.ARTIFACT}-${Build.VERSION}.jar")
-        assertTrue(ClassSet.from(jar.canonicalFile).mapNotNull { it.className }.toSet().isNotEmpty())
+        assertTrue(ClassSet.from(jar.canonicalFile, AsmClassFileParser).mapNotNull { it.className }.toSet().isNotEmpty())
     }
 
     @Test
     fun `create clustered class archives`() {
         val classes = File(javaClass.protectionDomain.codeSource.location.file)
-        val arch1 = ClassSet.from(classes)
+        val arch1 = ClassSet.from(classes, AsmClassFileParser)
         val jar = File(javaClass.protectionDomain.codeSource.location.file).resolve("..${File.separator}..${File.separator}..${File.separator}libs${File.separator}${Build.ARTIFACT}-${Build.VERSION}.jar")
-        val arch2 = ClassSet.from(jar.canonicalFile)
+        val arch2 = ClassSet.from(jar.canonicalFile, AsmClassFileParser)
 
         (arch1 + arch2).parallelStream().forEach {
             println(it.className)
@@ -35,22 +51,22 @@ class ClassSetTest {
     @Test
     fun `load clustered class archives`() {
         val classes = File(javaClass.protectionDomain.codeSource.location.file)
-        val arch1 = ClassSet.from(classes)
+        val arch1 = ClassSet.from(classes, AsmClassFileParser)
         val jar = File(javaClass.protectionDomain.codeSource.location.file).resolve("..${File.separator}..${File.separator}..${File.separator}libs${File.separator}${Build.ARTIFACT}-${Build.VERSION}.jar")
-        val arch2 = ClassSet.from(jar.canonicalFile)
-        (arch1 + arch2).use(ClassSet::load)
+        val arch2 = ClassSet.from(jar.canonicalFile, AsmClassFileParser)
+        (arch1 + arch2).use(ClassSet<ClassNode, AsmClassFileParser>::load)
     }
 
     @Test
     fun `load directory backed class archives`() {
         val classes = File(javaClass.protectionDomain.codeSource.location.file)
-        ClassSet.from(classes).use(ClassSet::load)
+        ClassSet.from(classes, AsmClassFileParser).use(ClassSet<ClassNode, AsmClassFileParser>::load)
     }
 
     @Test
     fun `load jar backed class archives`() {
         val jar = File(javaClass.protectionDomain.codeSource.location.file).resolve("..${File.separator}..${File.separator}..${File.separator}libs${File.separator}${Build.ARTIFACT}-${Build.VERSION}.jar")
-        ClassSet.from(jar.canonicalFile).use(ClassSet::load)
+        ClassSet.from(jar.canonicalFile, AsmClassFileParser).use(ClassSet<ClassNode, AsmClassFileParser>::load)
     }
 
 }
